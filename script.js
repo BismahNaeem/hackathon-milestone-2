63ef3c64406ebd0a752433b1818d70e4cd4b71d3
Bismah Naeem <bianaeem18@gmail.com>
2024-11-07 21:38:46 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var toggleButton = document.getElementById('toggleSkills');
+    var skillsList = document.getElementById('skillsList');
+    if (toggleButton && skillsList) {
+        toggleButton.addEventListener('click', function () {
+            if (skillsList.style.display === 'none') {
+                skillsList.style.display = 'block';
+            }
+            else {
+                skillsList.style.display = 'none';
+            }
+        });
+    }
+});
